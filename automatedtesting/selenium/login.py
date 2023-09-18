8de9f452f7dadba2114b9d494ef8f09957bfbec6
fodare <folorunsodamilare007@hotmail.com>
2023-09-18 10:22:14 +0200

    Update date format.

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -8,13 +8,13 @@ import datetime
 
 # Start the browser and login with standard_user
 def login(user, password):
-    print(f"{datetime.datetime.now()} (INFO): Starting the browser...")
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Starting the browser...")
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
-    print(f'{datetime.datetime.now()} (INFO): Browser started successfully. Navigating to the demo page to login.')
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Browser started successfully. Navigating to the demo page to login.")
     driver.get('https://www.saucedemo.com/')
     driver.find_element(By.NAME, 'user-name').send_keys(user)
     driver.find_element(By.NAME, "password").send_keys(password)
@@ -23,20 +23,20 @@ def login(user, password):
     title = driver.find_element(By.CLASS_NAME, "title").text
     assert title == "Products"
     if title == "Products":
-        print(f"{datetime.datetime.now()} (INFO): User loggedin successfully...!")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): User loggedin successfully...!")
     else:
-        print(f"{datetime.datetime.now()} (INFO): Error! User login failed...!")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (ERROR): Error! User login failed...!")
     time.sleep(3)
 
 
 def add_and_remove_cartItems(user, password):
     """Adds and removes cart items."""
-    print(f"{datetime.datetime.now()} (INFO): Starting cart item test....!")
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Starting cart item test....!")
     options = ChromeOptions()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options=options)
     # driver = webdriver.Chrome()
-    print(f'{datetime.datetime.now()} (INFO): Browser started successfully. Navigating to product catalog...!')
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Browser started successfully. Navigating to product catalog...!")
     driver.get('https://www.saucedemo.com/')
     driver.find_element(By.NAME, 'user-name').send_keys(user)
     driver.find_element(By.NAME, "password").send_keys(password)
@@ -44,9 +44,9 @@ def add_and_remove_cartItems(user, password):
     title = driver.find_element(By.CLASS_NAME, "title").text
     if title == "Products":
         print(
-            f"{datetime.datetime.now()} (INFO): Successfully navigated to the products page!")
+            f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Successfully navigated to the products page!")
     else:
-        print(f"{datetime.datetime.now()} (INFO): Error!. Can not load product catlog!")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (ERROR): Error!. Can not load product catlog!")
     driver.find_element(By.NAME, "add-to-cart-sauce-labs-backpack").click()
     driver.find_element(By.NAME, "add-to-cart-sauce-labs-bike-light").click()
     driver.find_element(By.NAME, "add-to-cart-sauce-labs-bolt-t-shirt").click()
@@ -55,18 +55,18 @@ def add_and_remove_cartItems(user, password):
     driver.find_element(By.NAME, "add-to-cart-sauce-labs-onesie").click()
     driver.find_element(
         By.NAME, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
-    print(f"{datetime.datetime.now()} (INFO): Added all products to cart.")
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Added all products to cart.")
 
     cart_item_count = driver.find_element(
         By.CLASS_NAME, "shopping_cart_badge").text
     assert int(cart_item_count) == 6
     if int(cart_item_count) == 6:
-        print(f"{datetime.datetime.now()} (INFO): Cart item count: {cart_item_count}")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Cart item count: {cart_item_count}")
     else:
         print(
-            f"{datetime.datetime.now()} (INFO): Error! Cart item count is not equal to 6!")
+            f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Error! Cart item count is not equal to 6!")
 
-    print(f"{datetime.datetime.now()} (INFO): Removing cart items .....!")
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Removing cart items .....!")
     driver.find_element(By.NAME, "remove-sauce-labs-backpack").click()
     driver.find_element(By.NAME, "remove-sauce-labs-bike-light").click()
     driver.find_element(By.NAME, "remove-sauce-labs-bolt-t-shirt").click()
@@ -76,9 +76,9 @@ def add_and_remove_cartItems(user, password):
     driver.find_element(
         By.NAME, "remove-test.allthethings()-t-shirt-(red)").click()
     if driver.find_element(By.CLASS_NAME, "shopping_cart_link"):
-        print(f"{datetime.datetime.now()} (INFO): Successfully removed cart items!")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Successfully removed cart items!")
     else:
-        print(f"{datetime.datetime.now()} (INFO): Error! Cart item not empty!.")
+        print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} (INFO): Error! Cart item not empty!.")
     time.sleep(2)
 
 
